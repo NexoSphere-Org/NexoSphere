208874d25cbcea7bf70f79651b2a4a0ca7b87f3b
barualee <barua.nishant97@gmail.com>
2024-10-31 23:59:01 -0400

    ec2 deploy

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,7 +1,7 @@
-output "aws_ec2_sg_details" {
+output "coen6313_project_sg_details" {
   value = aws_security_group.coen6313_project_sg
 }
 
-output "http_server_public_dns" {
+output "coen6313_project_public_dns" {
   value = aws_instance.coen6313_project_server.public_dns
 }
