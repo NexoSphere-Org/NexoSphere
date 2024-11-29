d4c8952577cc751eb31fa255080b6936c654c7f7
TheBarzani <ismaelmergasori@gmail.com>
2024-11-28 21:50:04 -0500

    updated the swaggerhub specifications

--- nexosphere/stockPriceControllers/stockPriceController.py
+++ nexosphere/stockPriceControllers/stockPriceController.py
@@ -2,18 +2,20 @@
 from flask_restx import Namespace, Resource, fields
 from flask import request
 from stockPriceControllers.stockPriceService import getStockPrice
+from datetime import datetime
 
-api = Namespace('stock_prices', description='Stock Price operations')
+api = Namespace('stock_prices', description='Stock Price operations',path='/')
 
 stock_price_model = api.model('StockPrice', {
     'ticker': fields.String(required=True, description='The stock ticker symbol')
 })
 
-@api.route('/getStockPrices')
+@api.route('/stockPrice/<ticker>', methods=['GET'])
 class GetStockPrices(Resource):
     @api.doc('get_stock_prices')
-    @api.param('ticker', 'The stock ticker symbol')
-    def get(self):
+    @api.param('start_date', 'The start date for fetching stock prices')
+    @api.param('end_date', 'The end date for fetching stock prices')
+    def get(self, ticker):
         """Fetch stock prices for a given ticker symbol"""
         ticker = ticker.upper()
         
@@ -24,6 +26,8 @@ class GetStockPrices(Resource):
         
         if result == {}:
             return {"message": "Ticker Data not found"}, 404
-        
+        for item in result:
+            if isinstance(result[item], datetime):
+                result[item] = result[item].isoformat()
         return result, 200
         
